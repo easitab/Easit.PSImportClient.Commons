da11d3cd90b33848a2ff664cb3a5b3f304e1794e
Anders Thyrsson <anders.thyrsson@easit.com>
2024-09-24 16:58:23 +0200

    add function Write-CustomLog with tests

---
+++ source/public/Write-CustomLog.ps1
@@ -0,0 +1,201 @@
+function Write-CustomLog {
+    <#
+    .SYNOPSIS
+        Writes input to file and a output stream.
+    .DESCRIPTION
+        This function provide the option to log output and / or progress in scripts.
+        While there are functions like *Start-Transcript* and *Out-File*, *Write-CustomLog* also handles log rotation and naming of log history.
+
+        *Write-CustomLog* will always append *_date* to the logname and remove logs older than the value of *RotationInterval*.
+
+        *Write-CustomLog* uses *Out-File* for writing output to a file and then redirects either *Message* or *InputObject* to the stream corresponding with the value of *Level*.
+
+        * If no input is provided for *-LogName*, *-LogDirectory* nor *-RotationInterval* the function will look for a variable named LoggerSettings in the global scope with a property or key with the same name and use that value.
+        * If no input is provided for *-LogName*, the name of the caller script is used as input.
+        * If no input is provided for *-LogDirectory*, logs will be written to $pwd.
+        * If no input is provided for *-RotationInterval*, 30 will used as value.
+    .PARAMETER Message
+        String that will be written to file and stream.
+    .PARAMETER InputObject
+        The object that will be written to file and stream.
+    .PARAMETER Level
+        What stream should the input be redirected to.
+    .PARAMETER OutputLevel
+        What level of input should be written to file and stream.
+    .PARAMETER LogName
+        Name of logfile.
+    .PARAMETER LogDirectory
+        In what directory should logs be saved.
+    .PARAMETER RotationInterval
+        For how many days should logs be kept on disk.
+    .PARAMETER Rotate
+        Tells the function to rotate logs. If this is always included with *Write-CustomLog* it will always try to rotate logs each time *Write-CustomLog* is invoked.
+    .EXAMPLE
+        Write-CustomLog -Message "Staring script"
+
+        In this example we write the string *Starting script* as a log entry with the level of INFO.
+        It will also use Write-Information to output it to the correct stream.
+    .EXAMPLE
+        Write-CustomLog -InputObject $_ -Level ERROR
+
+        In this example we write the current objekt to as a log entry with the level of ERROR.
+        It will also use Write-Error to output it to the correct stream.
+    .EXAMPLE
+        Write-CustomLog -Message "Rotating logs" -Level VERBOSE -Rotate
+
+        In this example we write the string *Starting script* as a log entry with the level of INFO.
+        It will also use Write-Information to output it to the correct stream.
+        Since we specify *-Rotate* the function will try to remove files older than set by *RotationInterval*.
+    .EXAMPLE
+        Write-CustomLog -Message "Starting script and rotating logs" -Rotate
+        Write-CustomLog -Message "Trying something" -Level VERBOSE
+        try {
+            try-something
+        } catch {
+            Write-CustomLog -InputObject $_ -Level ERROR
+            return
+        }
+        Write-CustomLog -Message "Script end"
+
+        Basic *real world* example of how to use *Write-CustomLog* in a script.
+    .INPUTS
+        [System.String](https://learn.microsoft.com/en-us/dotnet/api/system.string)
+
+        [System.Object](https://learn.microsoft.com/en-us/dotnet/api/system.object)
+    .OUTPUTS
+        None. This cmdlet returns no output
+    #>
+	[CmdletBinding(HelpUri='https://docs.easitgo.com/techspace/psmodules/psimportclientcommons/functions/writecustomlog/')]
+	Param (
+		[Parameter(ValueFromPipeline,ParameterSetName='string')]
+        [string]$Message,
+		[Parameter(ValueFromPipeline,ParameterSetName='object')]
+        [object]$InputObject,
+		[Parameter()]
+        [ValidateSet('ERROR','WARN','INFO','VERBOSE','DEBUG')]
+		[string]$Level = 'INFO',
+        [Parameter()]
+        [ValidateSet('ERROR','WARN','INFO','VERBOSE','DEBUG')]
+		[string]$OutputLevel,
+		[Parameter()]
+		[string]$LogName,
+		[Parameter()]
+		[string]$LogDirectory,
+		[Parameter()]
+		[int]$RotationInterval,
+        [Parameter()]
+        [switch]$Rotate
+	)
+    begin {
+        Write-Verbose "$($MyInvocation.MyCommand) begin"
+    }
+    process {
+        try {
+            $callStack = Get-PSCallStack
+            $function = $callStack[1].Command
+            $lineNumber = $MyInvocation.ScriptLineNumber
+        } catch {
+
+        }
+        if (!($null -eq (Get-Variable -Name 'psImportClient_loggerSettings' -ErrorAction 'SilentlyContinue'))) {
+            $loggerSettings = Get-Variable -Name 'psImportClient_loggerSettings' -ValueOnly
+        }
+        if ($loggerSettings.disabled -eq 'true') {
+            break
+        }
+        if ([string]::IsNullOrWhiteSpace($LogName)) {
+            if (!([string]::IsNullOrWhiteSpace($loggerSettings.LogName))) {
+                $LogName = $loggerSettings.LogName
+            } else {
+                $LogName = $callStack[1].Command.TrimEnd('\.ps1')
+            }
+        }
+        if ([string]::IsNullOrWhiteSpace($OutputLevel)) {
+            if (!([string]::IsNullOrWhiteSpace($loggerSettings.LoggingLevel))) {
+                $OutputLevel = $loggerSettings.LoggingLevel
+            } else {
+                $OutputLevel = 'INFO'
+            }
+        }
+        if ([string]::IsNullOrWhiteSpace($Level)) {
+            $Level = 'INFO'
+        }
+        $AdjustedLevel = $Level
+        switch($AdjustedLevel){
+            'INFO' {$AdjustedLevel = 'INFO   '}
+            'DEBUG' {$AdjustedLevel = 'DEBUG  '}
+            'ERROR' {$AdjustedLevel = 'ERROR  '}
+            'WARN' {$AdjustedLevel = 'WARN   '}
+        }
+        if ($Level -eq 'ERROR' -and !($psImportClient_notificationSettings.sendNotification) -and !($null -eq $psImportClient_notificationSettings)) {
+            $psImportClient_notificationSettings.sendNotification = $true
+        }
+        if ([string]::IsNullOrWhiteSpace($LogDirectory)) {
+            $curLoc = (Get-Location).Path
+            if (!([string]::IsNullOrWhiteSpace($loggerSettings.LogDirectory))) {
+                $LogDirectory = Join-Path -Path $curLoc -ChildPath $loggerSettings.LogDirectory
+            } else {
+                $LogDirectory = Join-Path -Path $curLoc -ChildPath 'logs'
+            }
+        }
+        if ([string]::IsNullOrWhiteSpace("$RotationInterval")) {
+            if (!([string]::IsNullOrWhiteSpace("$($loggerSettings.RotationInterval)"))) {
+                $RotationInterval = $loggerSettings.RotationInterval
+            } else {
+                $RotationInterval = 30
+            }
+        }
+        $LogLevelTable = @{
+            ERROR = 1
+            WARN = 2
+            INFO = 3
+            VERBOSE = 4
+            DEBUG = 5
+        }
+        $today = Get-Date -Format "yyyyMMdd"
+        $LogName = "${LogName}_${today}.log"
+        $logOutputPath = Join-Path -Path "$LogDirectory" -ChildPath "$LogName"
+        $outfileParams = @{
+            FilePath = "$logOutputPath"
+            Encoding = 'UTF8'
+            Append = $true
+            NoClobber = $true
+        }
+        if ($Level -eq 'ERROR' -and $null -eq $psImportClient_notificationSettings.logFilepath -and !($null -eq $psImportClient_notificationSettings)) {
+            $psImportClient_notificationSettings.logFilepath = $outfileParams.FilePath
+        }
+        if ($Rotate) {
+            $logArchiveFiles = Get-ChildItem -Path "$LogDirectory" -Recurse  -Include "*${logname}*.log"
+            foreach ($logArchiveFile in $logArchiveFiles) {
+                if ($logArchiveFile.CreationTime -lt ((Get-Date).AddDays(-$RotationInterval))) {
+                    "$($logArchiveFile.Name) is older than $RotationInterval days, removing.." | Out-File @outfileParams
+                    try {
+                        Remove-Item "$($logArchiveFile.FullName)" -Force
+                    } catch {
+                        Write-Error $_
+                        exit
+                    }
+                    "$FormattedDate - INFO   - Removed $($logArchiveFile.Name)" | Out-File @outfileParams
+                }
+            }
+        }
+        if ($LogLevelTable."$Level" -le $LogLevelTable."$OutputLevel") {
+            $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff"
+            $PSStyle.OutputRendering = 'PlainText'
+            if (!(Test-Path $logOutputPath)) {
+                $NewLogFile = New-Item "$logOutputPath" -Force -ItemType File
+                "$FormattedDate - INFO   - Created $NewLogFile [$function @ $lineNumber]" | Out-File @outfileParams
+            }
+            if ($Message) {
+                "$FormattedDate - $AdjustedLevel - $Message [$function @ $lineNumber]" | Out-File @outfileParams
+            }
+            if ($InputObject) {
+                "$FormattedDate - $AdjustedLevel - InputObject [$function @ $lineNumber]" | Out-File @outfileParams
+                $InputObject | Out-File @outfileParams
+            }
+        }
+    }
+    end {
+        Write-Verbose "$($MyInvocation.MyCommand) end"
+    }
+}
---
+++ tests/function/Write-CustomLog.Tests.ps1
@@ -0,0 +1,145 @@
+BeforeAll {
+    try {
+        $getEnvSetPath = Join-Path -Path (Split-Path -Path (Split-Path -Path $PSCommandPath -Parent) -Parent) -ChildPath 'getEnvironmentSetting.ps1'
+        . $getEnvSetPath
+        $envSettings = Get-EnvironmentSetting -Path $PSCommandPath
+    } catch {
+        throw $_
+    }
+    if (Test-Path $envSettings.CodeFilePath) {
+        . $envSettings.CodeFilePath
+    } else {
+        Write-Output "Unable to locate code file ($($envSettings.CodeFilePath)) to test against!" -ForegroundColor Red
+    }
+    function Out-File {
+        param (
+            [Parameter(ValueFromPipeline)]
+            [String]$String,
+            [Parameter()]
+            [String]$FilePath,
+            [Parameter()]
+            [String]$Encoding,
+            [Parameter()]
+            [Switch]$Force,
+            [Parameter()]
+            [Switch]$Append,
+            [Parameter()]
+            [Switch]$NoClobber
+        )
+    }
+}
+Describe "Write-CustomLog" -Tag 'function','public' {
+    It 'help section should have a SYNOPSIS' {
+        ((Get-Help "$($envSettings.CommandName)" -Full).SYNOPSIS).Length | Should -BeGreaterThan 0
+    }
+    It 'help section should have a DESCRIPTION' {
+        ((Get-Help "$($envSettings.CommandName)" -Full).DESCRIPTION).Length | Should -BeGreaterThan 0
+    }
+    It 'help section should have EXAMPLES' {
+        ((Get-Help "$($envSettings.CommandName)" -Full).EXAMPLES).Length | Should -BeGreaterThan 0
+    }
+    It 'should have a HelpUri' {
+        ((Get-Command "$($envSettings.CommandName)").HelpUri).Length | Should -BeGreaterThan 0
+    }
+    It 'all parameters should have a description' {
+        $commonParameters = [System.Management.Automation.PSCmdlet]::CommonParameters
+        $optionalCommonParameters = [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
+        foreach ($param in (Get-Help -Name "$($envSettings.CommandName)" -Full).parameters.parameter) {
+            if ($commonParameters -notcontains $param.name -and $optionalCommonParameters -notcontains $param.name) {
+                ($param.description.Text).Length | Should -BeGreaterThan 0
+            }
+        }
+    }
+    It 'should have a parameter named Message' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Message
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Message -Not -Mandatory
+    }
+    It 'and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Message -Type String
+    }
+    It 'should have a parameter named InputObject' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter InputObject
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter InputObject -Not -Mandatory
+    }
+    It 'and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter InputObject -Type Object
+    }
+    It 'should have a parameter named Level' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Level
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Level -Not -Mandatory
+    }
+    It 'it accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Level -Type String
+    }
+    It 'and have a default value = INFO' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Level -DefaultValue INFO
+    }
+    It 'should have a parameter named OutputLevel' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter OutputLevel
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter OutputLevel -Not -Mandatory
+    }
+    It 'and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter OutputLevel -Type String
+    }
+    It 'should have a parameter named LogName' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogName
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogName -Not -Mandatory
+    }
+    It 'and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogName -Type String
+    }
+    It 'should have a parameter named LogDirectory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogDirectory
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogDirectory -Not -Mandatory
+    }
+    It 'and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter LogDirectory -Type String
+    }
+    It 'should have a parameter named RotationInterval' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter RotationInterval
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter RotationInterval -Not -Mandatory
+    }
+    It 'and accepts an int' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter RotationInterval -Type Int
+    }
+    It 'should have a parameter named Rotate' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Rotate
+    }
+    It 'that is not mandatory' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Rotate -Not -Mandatory
+    }
+    It 'and accepts an int' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter Rotate -Type Switch
+    }
+    It 'should throw with invalid input' {
+        {Write-CustomLog} | Should -Throw
+    }
+    It 'should not throw with valid input' {
+        {Write-CustomLog -Message "test" -LogName 'testLog' -LogDirectory (Join-Path -Path $envSettings.ProjectDirectory -ChildPath 'logs')} | Should -Not -Throw
+    }
+    It 'should produce a log file' {
+        {Get-ChildItem -Path (Join-Path -Path $envSettings.ProjectDirectory -ChildPath 'logs') -Recurse -Filter 'testLog*'} | Should -Not -BeNullOrEmpty
+    }
+}
+AfterAll {
+    try {
+        Get-ChildItem -Path (Join-Path -Path $envSettings.ProjectDirectory -ChildPath 'logs') -Recurse -Filter 'testLog*' | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $envSettings.ProjectDirectory -Recurse -Directory -Include 'logs' | Remove-Item -Confirm:$false
+    } catch {
+        throw $_
+    }
+}
