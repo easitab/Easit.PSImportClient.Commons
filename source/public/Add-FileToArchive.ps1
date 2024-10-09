18dc7a05fdc896af1867905d19342397bb8be766
Anders Thyrsson <anders.thyrsson@easit.com>
2024-10-09 11:35:54 +0200

    add function Add-FileToArchive and tests

---
+++ source/public/Add-FileToArchive.ps1
@@ -0,0 +1,81 @@
+function Add-FileToArchive {
+    <#
+    .SYNOPSIS
+        Adds an file to the archive.
+    .DESCRIPTION
+        The **Add-FileToArchive** function adds a file to the archive by copying it from its original location.
+
+        Its new location is a combination of the destination name, source name, date and a new name in the combination of the time and original file name.
+
+        Ex: archive/test/jdbcSource/date/time_filename.fileextension
+    .PARAMETER ArchiveDirectory
+        Path to archive.
+    .PARAMETER FileToArchive
+        Path to file to archive.
+    .PARAMETER ConfigurationName
+        Name of destination configuration.
+    .PARAMETER SourceName
+        Name of source configuration.
+    .EXAMPLE
+        $addFileToArchiveParams = @{
+            FileToArchive = $FileToArchive
+            ArchiveDirectory = $ArchiveSettings.directory
+            ConfigurationName = $ConfigurationName
+            SourceName = $SourceName
+        }
+        Add-FileToArchive @addFileToArchiveParams
+    .INPUTS
+        [System.String](https://learn.microsoft.com/en-us/dotnet/api/system.string)
+    .OUTPUTS
+        None. This function returns no output.
+    #>
+    [CmdletBinding(HelpUri='https://docs.easitgo.com/techspace/psmodules/psimportclientcommons/functions/addfiletoarchive/')]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ArchiveDirectory,
+        [Parameter(Mandatory)]
+        [string]$FileToArchive,
+        [Parameter(Mandatory)]
+        [string]$ConfigurationName,
+        [Parameter(Mandatory)]
+        [string]$SourceName
+    )
+    
+    begin {
+        Write-CustomLog -Message "$($MyInvocation.MyCommand) initialized" -Level VERBOSE
+    }
+    
+    process {
+        if (Test-Path -Path $FileToArchive) {
+            $nowDate = Get-Date -Format "yyyy-MM-dd"
+            $nowTime = Get-Date -Format "HHmmss"
+            $fileObject = Get-ChildItem -Path $FileToArchive
+            $fileName = "$($fileObject.Name)"
+            $configurationArchiveDirectory = Join-Path -Path $ArchiveDirectory -ChildPath "$ConfigurationName"
+            if (!(Test-Path -Path $configurationArchiveDirectory)) {
+                $configurationArchiveDirectory = New-Item -Path $ArchiveDirectory -Name "$ConfigurationName" -ItemType Directory
+            }
+            $sourceArchiveDirectory = Join-Path -Path $configurationArchiveDirectory -ChildPath "$SourceName"
+            if (!(Test-Path -Path $sourceArchiveDirectory)) {
+                $sourceArchiveDirectory = New-Item -Path $configurationArchiveDirectory -Name "$SourceName" -ItemType Directory
+            }
+            $dateSourceArchiveDirectory = Join-Path -Path $sourceArchiveDirectory -ChildPath "$nowDate"
+            if (!(Test-Path -Path $dateSourceArchiveDirectory)) {
+                $dateSourceArchiveDirectory = New-Item -Path $sourceArchiveDirectory -Name $nowDate -ItemType Directory
+            }
+            $destinationFile = Join-Path -Path $dateSourceArchiveDirectory -ChildPath "${nowTime}_${fileName}"
+            try {
+                Copy-Item -Path $fileObject.FullName -Destination $destinationFile
+            } catch {
+                throw
+            }
+        } else {
+            throw "Unable to find $FileToArchive"
+        }
+    }
+    
+    end {
+        Write-CustomLog -Message "$($MyInvocation.MyCommand) end" -Level VERBOSE
+    }
+}
---
+++ tests/function/Add-FileToArchive.Tests.ps1
@@ -0,0 +1,107 @@
+BeforeAll {
+    $helpersDirectory = Join-Path -Path (Split-Path -Path (Split-Path -Path $PSCommandPath -Parent) -Parent) -ChildPath 'helpers'
+    foreach ($helper in (Get-ChildItem -Path $helpersDirectory -Recurse -Include '*.ps1')) {
+        . $helper.FullName
+    }
+    try {
+        $envSettings = Get-EnvironmentSetting -Path $PSCommandPath
+    } catch {
+        throw $_
+    }
+    if (Test-Path $envSettings.CodeFilePath) {
+        . $envSettings.CodeFilePath
+    } else {
+        Write-Output "Unable to locate code file ($($envSettings.CodeFilePath)) to test against!" -ForegroundColor Red
+    }
+    $archiveDirectoryName1 = 'archive1'
+    $archiveDirectoryName2 = 'archive2'
+    $configName1 = 'configName1'
+    $configName2 = 'configName2'
+    $sourceName1 = 'sourceName1'
+    $sourceName2 = 'sourceName2'
+    $fileToArchive = Join-Path -Path $envSettings.TestDataDirectory -ChildPath "$($envSettings.LogPrefix)-temp"
+    function Write-CustomLog {
+        param (
+            [string]$Message,
+            [object]$InputObject,
+            [string]$Level = 'INFO',
+            [string]$OutputLevel,
+            [string]$LogName,
+            [string]$LogDirectory,
+            [int]$RotationInterval,
+            [switch]$Rotate
+        )
+        $outFilePath = "$($envSettings.ErrorFilePath).${Level}"
+        if ($InputObject) {
+            $InputObject | Out-File -FilePath $outFilePath -Force -Append
+        }
+        if ($Message) {
+            $Message | Out-File -FilePath $outFilePath -Force -Append
+        }
+    }
+}
+Describe "Add-FileToArchive" -Tag 'function','public'{
+    BeforeEach {
+        if (Test-Path $fileToArchive) {
+
+        } else {
+            $null = New-Item -Path $fileToArchive -ItemType File -Force
+        }
+    }
+    It 'should have complete help section' {
+        {Test-HelpSection -CommandName "$($envSettings.CommandName)"} | Should -Not -Throw
+    }
+    It 'should have a parameter named ArchiveDirectory that is mandatory and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter ArchiveDirectory -Mandatory -Type [String]
+    }
+    It 'should have a parameter named FileToArchive that is mandatory and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter FileToArchive -Mandatory -Type [String]
+    }
+    It 'should have a parameter named ConfigurationName that is mandatory and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter ConfigurationName -Mandatory -Type [String]
+    }
+    It 'should have a parameter named SourceName that is mandatory and accepts a string' {
+        Get-Command "$($envSettings.CommandName)" | Should -HaveParameter SourceName -Mandatory -Type [String]
+    }
+    It 'should throw if file to archive does not exist' {
+        $tempParams = @{
+            ArchiveDirectory = $envSettings.TestDataDirectory
+            FileToArchive = (Join-Path -Path $envSettings.TestDataDirectory -ChildPath "$($envSettings.LogPrefix)-temp2")
+            ConfigurationName = $configName1
+            SourceName = $sourceName1
+        }
+        {Add-FileToArchive @tempParams} | Should -Throw
+    }
+    It 'should NOT throw' {
+        $tempParams = @{
+            ArchiveDirectory = $envSettings.TestDataDirectory
+            FileToArchive = $fileToArchive
+            ConfigurationName = $configName1
+            SourceName = $sourceName1
+        }
+        {Add-FileToArchive @tempParams} | Should -Not -Throw
+    }
+    It 'should copy a file to archive' {
+        $tempParams = @{
+            ArchiveDirectory = $envSettings.TestDataDirectory
+            FileToArchive = $fileToArchive
+            ConfigurationName = $configName1
+            SourceName = $sourceName1
+        }
+        Add-FileToArchive @tempParams
+        Get-ChildItem -Path (Join-Path -Path $envSettings.TestDataDirectory -ChildPath $configName1) -Include "*$($envSettings.LogPrefix)-temp*" -Recurse -File | Should -Not -BeNullOrEmpty
+    }
+    AfterEach {
+        $tempParams = $null
+        Remove-Item -Path $fileToArchive -Confirm:$false -ErrorAction SilentlyContinue -Force
+        Get-ChildItem -Path (Join-Path -Path $envSettings.TestDataDirectory -ChildPath $configName1) -Recurse -File | Remove-Item -Confirm:$false -Force
+        Get-ChildItem -Path $envSettings.TestDataDirectory -Recurse -Include $configName1 -Directory -Force | Remove-Item -Confirm:$false -Force -Recurse
+    }
+}
+AfterAll {
+    try {
+        Get-ChildItem -Path $envSettings.TestDataDirectory -Recurse -Include "$($envSettings.LogPrefix)*" -File | Remove-Item -Confirm:$false
+    } catch {
+        throw
+    }
+}
